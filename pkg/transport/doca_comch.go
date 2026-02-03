21555d22e2a93a7ecdcb1a69e94e7d00c6a90eb1
Nelson Melo <nmelo@users.noreply.github.com>
2026-02-02 22:07:08 -0500

    Add libnl3 to DOCA CGO LDFLAGS
    DOCA SDK depends on libnl-3 and libnl-genl-3 for netlink operations.
    Required for ComCh transport linking.
    
    Fixes: si-qir.7.8

--- pkg/transport/doca_comch.go
+++ pkg/transport/doca_comch.go
@@ -4,7 +4,7 @@
 package transport
 
 /*
-#cgo LDFLAGS: -ldoca_comch -ldoca_argp -ldoca_common -lstdc++
+#cgo LDFLAGS: -ldoca_comch -ldoca_argp -ldoca_common -lstdc++ -lnl-3 -lnl-genl-3
 
 #include <doca_comch.h>
 #include <doca_comch_consumer.h>
